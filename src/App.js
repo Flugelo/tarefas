4295c23c1d0e69052b8996c7344bb31a3c235f9f
Flugelo <gabrielflugel@gmail.com>
2023-02-05 19:05:53 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {useState, useEffect} from "react";
+import {BsTrash, BsBookmarkCheck, BsBookmarkeCheckFill} from "react-icons";
+
+const API = "http://localhost:5000"
+
 function App() {
+  const [title, setTitle] = useState("");
+  const [time, setTime] = useState(""); 
+  const [todos, setTodos] = useState([]);
+
+  const [loading, setLoading] = useState(false);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='todos-header'>
+        <h1>React Tarefas</h1>
+      </div>
+      <div className='form-todo'>
+        <p>Formúlario</p>
+      </div>
+      <div className='list-todo'>
+        <p>Listas</p>
+      </div>
     </div>
   );
 }
